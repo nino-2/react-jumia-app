1b6958ee0de6db7e394e3cde4e8f2129a3f4c2ce
Morgan Opeyemi <ayodeleopeyemi09@gmail.com>
2025-03-11 17:51:54 +0100

    'latest'

--- src/pages/AboutComponent.jsx
+++ src/pages/AboutComponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../component/Navbar'
-import Detail from '../component/detail'
+import Detail from '../component/Detail'
 import Footer from '../component/Footer'
 
 
