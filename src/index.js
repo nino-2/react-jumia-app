23aace723d454ec69a8f9560e352a6f74b942f2a
Morgan Opeyemi <ayodeleopeyemi09@gmail.com>
2026-06-03 15:22:18 +0100

    updated time

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-const countDownDate = new Date("April 02,2025 10:24:09").getTime();
+const countDownDate = new Date("July 02,2026 10:24:09").getTime();
 const x = setInterval(() => {
   const now = new Date().getTime();
   const distance = countDownDate - now;
   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
   const hours = Math.floor(
-    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
   );
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
